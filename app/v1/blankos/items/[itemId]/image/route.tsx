cb95dc34840dbf65140cea34e370181429c90066
Fyzz <110374089+keagancoding@users.noreply.github.com>
2023-09-11 18:51:58 -0700

    testing another fix

--- app/v1/blankos/items/[itemId]/image/route.tsx
+++ app/v1/blankos/items/[itemId]/image/route.tsx
@@ -9,16 +9,12 @@ async function getItemById(id: string) {
     headers: {
       accept: "application/json",
       "accept-language": "en-US,en;q=0.6",
-      authorization: "",
       "content-type": "application/json",
       "mythical-environment-id": "hcihi.p.535",
     },
-    referrer: "https://blankos.mythical.market/",
-    referrerPolicy: "strict-origin-when-cross-origin",
-    body: `{"query":"\\n  query GetItem($game_inventory_id: String!) {\\n    item: item(gameInventoryId: $game_inventory_id) {\\n      ...ItemView\\n    }\\n  }\\n  \\n  fragment ItemView on Item {\\n    game_inventory_id\\n    game_item_type_id\\n    dgood_id\\n    dgood_serial\\n    owned_pending_asks: asks(\\n      filter: {\\n        player_ask: true\\n        status: { operator: IN, value: [PENDING_CONFIRMED] }\\n      }\\n      page: { page_num: 0, page_size: 10 }\\n    ) {\\n      returning {\\n        id\\n        price\\n        currency\\n        status\\n      }\\n    }\\n    asks: asks(\\n      filter: { status: { operator: IN, value: [CONFIRMED] } }\\n      page: { page_num: 0, page_size: 10 }\\n    ) {\\n      returning {\\n        id\\n        price\\n        currency\\n        status\\n      }\\n    }\\n    metadata\\n    status\\n    cooldown_timestamp\\n    item_type {\\n      game_item_type_id\\n      item_class\\n      display_name\\n      description\\n      total_minted\\n      total_burned\\n      max_supply\\n      img_large_url\\n      img_thumb_url\\n      metadata\\n      withdrawable\\n    min_price_usd\\n    }\\n  }\\n\\n","variables":{"game_inventory_id":"${id}"}}`,
+    referrer: "https://api.mythicalinsider.com",
     method: "POST",
-    mode: "cors",
-    credentials: "include",
+    body: `{"query":"\\n  query GetItem($game_inventory_id: String!) {\\n    item: item(gameInventoryId: $game_inventory_id) {\\n      ...ItemView\\n    }\\n  }\\n  \\n  fragment ItemView on Item {\\n    game_inventory_id\\n    game_item_type_id\\n    dgood_id\\n    dgood_serial\\n    owned_pending_asks: asks(\\n      filter: {\\n        player_ask: true\\n        status: { operator: IN, value: [PENDING_CONFIRMED] }\\n      }\\n      page: { page_num: 0, page_size: 10 }\\n    ) {\\n      returning {\\n        id\\n        price\\n        currency\\n        status\\n      }\\n    }\\n    asks: asks(\\n      filter: { status: { operator: IN, value: [CONFIRMED] } }\\n      page: { page_num: 0, page_size: 10 }\\n    ) {\\n      returning {\\n        id\\n        price\\n        currency\\n        status\\n      }\\n    }\\n    metadata\\n    status\\n    cooldown_timestamp\\n    item_type {\\n      game_item_type_id\\n      item_class\\n      display_name\\n      description\\n      total_minted\\n      total_burned\\n      max_supply\\n      img_large_url\\n      img_thumb_url\\n      metadata\\n      withdrawable\\n    min_price_usd\\n    }\\n  }\\n\\n","variables":{"game_inventory_id":"${id}"}}`,
   });
 
   const item_data = await itemIdReq.json().catch((e) => {
@@ -49,6 +45,27 @@ export async function GET(
     if (!item) {
       throw new Error("Item not found");
     }
+
+    const inter = await fetch(
+      "https://api.mythicalinsider.com/fonts/Inter-Regular.ttf",
+      {
+        headers: {
+          referrer: "https://api.mythicalinsider.com",
+          referrerPolicy: "strict-origin-when-cross-origin",
+          mode: "cors",
+        },
+      }
+    ).then((res) => res.arrayBuffer());
+    const interBold = await fetch(
+      "https://api.mythicalinsider.com/fonts/Inter-Bold.ttf",
+      {
+        headers: {
+          referrer: "https://api.mythicalinsider.com",
+          referrerPolicy: "strict-origin-when-cross-origin",
+          mode: "cors",
+        },
+      }
+    ).then((res) => res.arrayBuffer());
 
     return new ImageResponse(
       (
@@ -113,6 +130,20 @@ export async function GET(
       {
         width: 500,
         height: 500,
+        fonts: [
+          {
+            name: "Inter",
+            data: inter,
+            weight: 400,
+            style: "normal",
+          },
+          {
+            name: "Inter",
+            data: interBold,
+            weight: 700,
+            style: "normal",
+          },
+        ],
       }
     );
   } catch (e) {
