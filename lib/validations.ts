53e6d72db403555384b5fee60d8ace1cdbbcc019
Fyzz <110374089+keagancoding@users.noreply.github.com>
2023-09-12 18:36:58 -0700

    pagination page 0

--- lib/validations.ts
+++ lib/validations.ts
@@ -2,16 +2,17 @@ import * as z from "zod";
 
 export const pageLimitSchema = z.object({
   page: z
-    .preprocess((a) => parseInt(z.string().parse(a), 10), z.number().positive())
+    .preprocess((a) => parseInt(z.string().parse(a)), z.number().min(0))
     .optional(),
   limit: z
-    .preprocess((a) => parseInt(z.string().parse(a), 10), z.number().positive())
+    .preprocess((a) => parseInt(z.string().parse(a)), z.number().min(1))
     .optional(),
 });
 
 export const collectionsSearchSchema = z.object({
   query: z.string(),
-  ...pageLimitSchema.shape,
+  page: pageLimitSchema.shape.page,
+  limit: pageLimitSchema.shape.limit,
 });
 
 export const collectionsUpdateSchema = z.object({
